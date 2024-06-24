0b16a4c5f9e5f0bb112a2541f7f6c7c5d4505ecd
Nishu <nishupriya8699@gmail.com>
2024-06-24 12:29:20 +0530

    minor responsive changes

--- script.js
+++ script.js
@@ -87,10 +87,4 @@ function deleteTask(button) {
 }
 
 
-document.querySelector('.container').addEventListener('scroll', function() {
-    if (this.scrollTop > 0) {
-        this.classList.add('scrolled');
-    } else {
-        this.classList.remove('scrolled');
-    }
-});
+
